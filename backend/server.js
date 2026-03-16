8cf1951d29768ab24b2380b3637892d40dc2d1f7
Payal00028 <payalmaletha2838@gmail.com>
2026-03-16 20:33:03 +0530

    Setup Express backend with health check API

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-
+const healthRoutes = require("./routes/healthRoutes");
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("ArtisansChain Backend Running");
 });
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.use("/api", healthRoutes);
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
