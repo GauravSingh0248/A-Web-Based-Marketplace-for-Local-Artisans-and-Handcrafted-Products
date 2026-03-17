f71325a72758ebe79b5c5b58c39bc41ba70985cf
RitikBhandari04 <bhandariritik825@gmail.com>
2026-03-17 10:08:30 +0530

    file updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import { Navbar } from './Components/Navbar/Navbar'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Shop from './Pages/Shop'
+import ShopCategory from './Pages/ShopCategory'
+import { Product } from './Pages/Product'
+import { LoginSignup } from './Pages/LoginSignup'
+import { Cart } from './Pages/Cart'
+import Footer from './Components/Footer/Footer'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Navbar></Navbar>
-    </>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/pottery" element={<ShopCategory category="pottery" />} />
+          <Route path="/textiles" element={<ShopCategory category="textiles" />} />
+          <Route path="/painting" element={<ShopCategory category="painting" />} />
+          <Route path="/product/:productId" element={<Product />} />
+
+          <Route path="/cart" element={<Cart />} />
+
+          <Route path="/login" element={<LoginSignup />} />
+
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </div>
   )
 }
 
